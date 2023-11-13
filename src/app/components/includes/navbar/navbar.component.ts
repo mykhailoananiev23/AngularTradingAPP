45090f02683543dc4b9bde7a90b571b0715ce8b8
mykhailoananiev23 <mykhailoananiev23@gmail.com>
2023-11-13 16:54:06 +0200

    add all components without modal

--- src/app/components/includes/navbar/navbar.component.ts
+++ src/app/components/includes/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  sessionActive = false;
 
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.checkSession();
+  }
+
+  checkSession() {
+    console.log(this.router.url)
+    this.sessionActive = true;
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
+  progressValue = 0;
+  showProgressBar = false;
 
+  toggleProgressBar() {
+    this.showProgressBar = !this.showProgressBar;
+    if (this.showProgressBar) {
+      this.animateProgressBar();
+    }
+  }
+
+  animateProgressBar() {
+    this.router.navigateByUrl('/dashboard');
+  }
 }
